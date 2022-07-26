ed1e31dcd1748442e90641360b1face86538334c
Aniket Yadav <aniket4897@gmail.com>
2022-07-26 20:34:06 +0530

    fix token generate

--- generate_token.php
+++ generate_token.php
@@ -1,23 +1,15 @@
 <?php
-/*
-* import checksum generation utility
-* You can get this utility from https://developer.paytm.com/docs/checksum/
-*/
 require_once("PaytmChecksum.php");
 include 'paytm_credentials.php';
-//  you can get this from from paytm decumentation
-// also  , this is in php there are other also
-//  i have just made some change here for good response
-
-$amount = $_POST["amount"]; // amount is coming from frontend
-
-
+// $amount = $_POST["amount"]; 
+$amount = 20;
 $paytmParams = array();
 $orderId = "ORDERID_".mt_rand(); // generate order id
 $paytmParams["body"] = array(
  "requestType"  => "Payment",
- "mid"  =>  $merchantId, // this you will get from dashboard of paytm
- "websiteName"  => "WEBSITE",
+ "mid"  =>  $merchantId, // this you will get from  paytm dashboard
+ "websiteName"  => "WEBSTAGING",
+ "industryType" => "Retail",
  "orderId"  => $orderId,
  "callbackUrl"  => "https://securegw-stage.paytm.in/theia/paytmCallback?ORDER_ID=".$orderId,
  "txnAmount"  => array(
@@ -26,31 +18,25 @@ $paytmParams["body"] = array(
  ),
  "userInfo" => array(
  "custId" => "CUST_".mt_rand(), // customer id
-
+"mobile"=> "7777777777",
  ),
-); // this is body 
+); // request body for txTOken generation 
 
-/*
-* Generate checksum by parameters we have in body
-* Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys 
-*/
-$checksum = PaytmChecksum::generateSignature(json_encode($paytmParams["body"], JSON_UNESCAPED_SLASHES), $marchantKey);
-//  this is to create checksum
+$checksum = PaytmChecksum::generateSignature(json_encode($paytmParams["body"], JSON_UNESCAPED_SLASHES), $merchantKey);
+
 $paytmParams["head"] = array(
- "signature" => $checksum // checksum is signature 
+ "signature" => $checksum // pass check in head , checksum in basically is signature.
 );
 
 $post_data = json_encode($paytmParams, JSON_UNESCAPED_SLASHES);
 
+// this url is to generate txtoken , you have to generate checksum and pass it in header and body 
 /* for Staging */
 $url = "https://securegw-stage.paytm.in/theia/api/v1/initiateTransaction?mid=$merchantId&orderId=$orderId";
-
-// this url is to generate txtoken , you have to generate checksum and pass it in header and body 
-
 /* for Production */
-// $url = "https://securegw.paytm.in/theia/api/v1/initiateTransaction?mid=YOUR_MID_HERE&orderId=ORDERID_98765";
+// $url = "https://securegw.paytm.in/theia/api/v1/initiateTransaction?mid=$merchantId&orderId=$orderId";
 
-//  use curl to call api and get reponse
+//  use curl to call api and get reponse from paytm server
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
@@ -63,8 +49,11 @@ if($res_json["body"]["resultInfo"]["resultCode"] == "0000"){ // 0000 means succe
 
     $response = array();
     $response["mid"] = $merchantId;
-    $response["txToken"]= $res_json["body"]["txnToken"];
+    $response["txnToken"]= $res_json["body"]["txnToken"];
     $response["orderId"] = $orderId;
+    $response["res"] = $res;
+    $response["paytm"]= $paytmParams;
+    $response["key"]=$merchantKey;
 echo json_encode($response);
 
 }else{
@@ -72,4 +61,5 @@ echo json_encode($response);
 http_response_code($res_json["body"]["resultInfo"]["resultCode"]);
     echo $res_json["body"]["resultInfo"]["resultMsg"];
 }
+// var_dump($res);
 ?>
